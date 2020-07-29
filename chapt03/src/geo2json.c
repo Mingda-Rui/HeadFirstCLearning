67a5fe7438bb6ce524a171a9022be947a21cb54c
Mingda Rui <mingdarui@gmail.com>
2020-07-29 23:45:08 +0100

    small tools for geo to json conversion

---
+++ chapt03/src/geo2json.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    float latitude;
+    float longitude;
+    char info[80];
+    int started = 0;
+
+    puts("data=[");
+    // %79[^\n] is just a way of saying, "Give me every 
+    // character up to the end of the line."
+    // Or, according to StackOverflow, The format "%79[^\n]" says all the characters up to 79 characters max that are no '\n'.
+
+    // The scanf() function returns the nuber of
+    // values it was able to read.
+    while (scanf("%f,%f,%79[^\n]", &latitude, &longitude, info) == 3) {
+        if (started)
+            printf(",\n");
+        else
+            started = 1;
+        printf("{latitude: %f, logitude: %f, info: '%s}", latitude, longitude, info);
+    }
+    puts("\n]");
+    return 0;
+}
