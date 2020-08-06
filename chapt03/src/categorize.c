b7877a1dcb0d299cf5ddd8f5fac40835d90b129e
Mingda Rui <mingdarui@gmail.com>
2020-08-06 23:35:33 +0100

    main function with parameters

--- chapt03/src/categorize.c
+++ chapt03/src/categorize.c
@@ -2,19 +2,37 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main()
+char tmp_dir_holder[30];
+
+void assign_file_relative_path_to_holder(char* file) {
+    strcpy(tmp_dir_holder, "./resources/");
+    strcat(tmp_dir_holder, file);
+}
+
+int main(int argc, char *argv[])
 {
     char line[80];
+    
+    if (argc != 6) {
+        fprintf(stderr, "You need to give 5 arguments\n");
+        return 1;
+    }
+    
+    assign_file_relative_path_to_holder("spooky.csv");
     // This will create a data stream to read from a file.
-    FILE *in = fopen("./resources/spooky.csv", "r");
+    FILE *in = fopen(tmp_dir_holder, "r");
+
     // This will create a data stream to write to a file.
-    FILE *file1 = fopen("./resources/ufos.csv", "w");
-    FILE *file2 = fopen("./resources/disappearances.csv", "w");
-    FILE *file3 = fopen("./resources/others.csv", "w");
+    assign_file_relative_path_to_holder(argv[2]);
+    FILE *file1 = fopen(tmp_dir_holder, "w");
+    assign_file_relative_path_to_holder(argv[4]);
+    FILE *file2 = fopen(tmp_dir_holder, "w");
+    assign_file_relative_path_to_holder(argv[5]);
+    FILE *file3 = fopen(tmp_dir_holder, "w");
     while ( fscanf(in, "%79[^\n]\n", line) == 1) {
-        if (strstr(line, "UFO"))
+        if (strstr(line, argv[1]))
             fprintf(file1, "%s\n", line);
-        else if (strstr(line, "Disappearance"))
+        else if (strstr(line, argv[3]))
             fprintf(file2, "%s\n", line);
         else
             fprintf(file3, "%s\n", line);
