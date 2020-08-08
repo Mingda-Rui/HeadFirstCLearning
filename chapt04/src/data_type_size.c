0c8099633ef5d4861c4ef82e02fa09b7a5ae72d1
Mingda Rui <mingdarui@gmail.com>
2020-08-08 23:23:15 +0100

    size of char double short long

--- chapt04/src/data_type_size.c
+++ chapt04/src/data_type_size.c
@@ -8,11 +8,28 @@ int main()
 {
     printf("The value of INT_MAX is %i\n", INT_MAX);
     printf("The value of INT_MIN is %i\n", INT_MIN);
-    printf("An int takes %zu bytes\n", sizeof(int));
+    printf("An int takes %zu bytes\n\n", sizeof(int));
 
     printf("The value of FLT_MAX is %f\n", FLT_MAX);
     printf("The value of FLT_MIN is %.50f\n", FLT_MIN);
-    printf("A float takes %zu bytes\n", sizeof(float));
+    printf("A float takes %zu bytes\n\n", sizeof(float));
+
+    printf("The value of CHAR_MAX is %i\n", CHAR_MAX);
+    printf("The value of CHAR_MIN is %i\n", CHAR_MIN);
+    printf("An char takes %zu bytes\n\n", sizeof(char));
+
+    printf("The value of DBL_MAX is %f\n", DBL_MAX);
+    printf("The value of DBL_MIN is %f\n", DBL_MIN);
+    printf("An double takes %zu bytes\n\n", sizeof(double));
+
+
+    printf("The value of SHRT_MAX is %i\n", SHRT_MAX);
+    printf("The value of SHRT_MIN is %i\n", SHRT_MIN);
+    printf("An short takes %zu bytes\n\n", sizeof(short));
+
+    printf("The value of LONG_MAX is %li\n", LONG_MAX);
+    printf("The value of LONG_MIN is %li\n", LONG_MIN);
+    printf("An long takes %zu bytes\n\n", sizeof(long));
 
     return 0;
 }
