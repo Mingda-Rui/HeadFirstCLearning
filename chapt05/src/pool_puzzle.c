89494a798c4335163d563921eca47bbd333094e8
Mingda Rui <mingdarui@gmail.com>
2020-08-15 18:51:03 +0100

    label with struct paramter

--- chapt05/src/pool_puzzle.c
+++ chapt05/src/pool_puzzle.c
@@ -13,6 +13,12 @@ void catalog(struct fish f)
         f.name, f.species, f.teeth, f.age);
 }
 
+void label(struct fish f)
+{
+    printf("Name;%s\nSpecies:5s\n%i years old, %i teeth\n",
+        f.name, f.species, f.age, f.teeth);
+}
+
 int main()
 {
     struct fish snappy = {"Snappy", "Piranha", 69, 4};
