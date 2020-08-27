7d8218a7f6e1724ac2e48ec51a4cab3e4abf08dd
Mingda Rui <mingdarui@gmail.com>
2020-08-27 22:38:15 +0100

    separate test (main function) from the c file

--- chapt06/src/tour.c
+++ chapt06/src/tour.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include "tour.h"
 
-void display(island *start);
-
 void display(island *start)
 {
     island *i = start;
@@ -10,20 +8,4 @@ void display(island *start)
     for (; i != NULL; i = i->next) {
         printf("Name: %s open: %s-%s\n", i->name, i->opens, i->closes);
     }
-}
-
-int main()
-{
-    island amity = {"Amity", "09:00", "17:00", NULL};
-    island craggy = {"Craggy", "09:00", "17:00", NULL};
-    island isla_nublar = {"Isla Nublar", "09:00", "17:00", NULL};
-    island shutter = {"Shutter", "09:00", "17:00", NULL};
-
-    amity.next = &craggy;
-    craggy.next = &isla_nublar;
-    isla_nublar.next = &shutter;
-    island skull = {"Skull", "09:00", "17:00", NULL};
-    isla_nublar.next = &skull;
-    skull.next = &shutter;
-    display(&amity);
 }
