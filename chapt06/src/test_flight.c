132f526885efefd0467213e8c6374615720f37dc
Mingda Rui <mingdarui@gmail.com>
2020-08-29 21:07:43 +0100

    stop reading when enter next line

--- chapt06/src/test_flight.c
+++ chapt06/src/test_flight.c
@@ -6,22 +6,25 @@
 island* create(char *name)
 {
     island *i = malloc(sizeof(island));
-    i -> name = strdup(name);
-    i -> opens = "09:00";
-    i -> closes = "17:00";
-    i -> next = NULL;
+    i->name = strdup(name);
+    i->opens = "09:00";
+    i->closes = "17:00";
+    i->next = NULL;
     return i;
 }
 
 int main()
 {
+    island *start = NULL;
+    island *i = NULL;
+    island *next = NULL;
     char name[80];
-    fgets(name, 80, stdin);
-    island *p_island0 = create(name);
-
-    fgets(name, 80, stdin);
-    island *p_island1 = create(name);
-    p_island0 -> next = p_island1;
-
-    display(p_island0);
+    for(; strcmp(fgets(name, 80, stdin), "\n") != 0; i = next) {
+        next = create(name);
+        if (start == NULL)
+            start = next;
+        if (i != NULL)
+            i->next = next;
+    }
+    display(start);
 }
