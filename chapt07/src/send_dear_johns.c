cf1619930b3cf6d4a9a3e67e85d9997ee4ebe7ed
Mingda Rui <mingdarui@gmail.com>
2020-09-05 18:14:58 +0100

    useage of function array with enum

---
+++ chapt07/src/send_dear_johns.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+enum response_type {DUMP, SECOND_CHANCE, MARRIAGE};
+
+typedef struct {
+    char *name;
+    enum response_type type;
+} response;
+
+void dump(response r)
+{
+    printf("Dear %s,\n", r.name);
+    puts("Unfortunately your last date contacted us to");
+    puts("say that they will not be seeing you again");
+}
+
+void second_chance(response r)
+{
+    printf("Dear %s,\n", r.name);
+    puts("Good news: your last date has asked us to");
+    puts("arrange another meetings. Please call ASAP.");
+}
+
+void marriage(response r)
+{
+    printf("Dear %s,\n", r.name);
+    puts("Congratulations! Your last date has contacted");
+    puts("us with a proposal of marriage.");
+}
+
+void (*replies[])(response) = {dump, second_chance, marriage};
+
+int main()
+{
+    response r[] = {
+        {"Mike", DUMP}, {"Luis", SECOND_CHANCE},
+        {"Matt", SECOND_CHANCE}, {"William", MARRIAGE}
+    };
+    int i;
+    for (i = 0; i < 4; i++) {
+        // If you wanted, you could have added a *
+        // after the opening parenthesis, but it would
+        // work the same way.
+        // (*replies[r[i].type])(r[i]);
+        (replies[r[i].type])(r[i]);
+    }
+    return 0;
+}
