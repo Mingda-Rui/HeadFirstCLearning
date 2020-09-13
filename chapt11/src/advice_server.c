f62203847c322498b7619202256edd9620cf5365
Mingda Rui <mingdarui@gmail.com>
2020-09-13 23:48:24 +0100

    advice list for the server

---
+++ chapt11/src/advice_server.c
@@ -0,0 +1,10 @@
+int main(int argc, char *argv[])
+{
+    char *advice[] ={
+        "Take smaller bites\r\n",
+        "Go for the tight jeans. No they do NOT make you look fat.\r\n",
+        "One word: inappropriate\r\n",
+        "Just for today, be honest. Tell your boss what you *really* think\r\n",
+        "You might want to rethink that haircut\r\n"
+    };
+}
