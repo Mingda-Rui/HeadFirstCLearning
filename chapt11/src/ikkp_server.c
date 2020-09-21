401a35b43516c097896e858ab36a473b8f3631f0
Mingda Rui <mingdarui@gmail.com>
2020-09-21 23:32:24 +0100

    use fork to have a separate process to hanld the secondary socket

--- chapt11/src/ikkp_server.c
+++ chapt11/src/ikkp_server.c
@@ -85,21 +85,32 @@ int main_hfc(int argc, char *argv[])
         int connect_d = accept(listener_d, (struct sockaddr *)&client_addr, &address_size);
         if (connect_d == -1)
             error("Can't open scondary socket");
-        if (say(connect_d, "Internet Knock-knock Protocol Server\r\nVersion I.O\r\nKnock!Knock!\r\n> ") != -1) {
-            read_in(connect_d, buf, sizeof(buf));
-            if( strncasecmp("Who's there?", buf, 12))
-                say(connect_d, "You should say 'Who's there?'!");
-            else {
-                if (say(connect_d, "Oscar\r\n> ") != -1) {
-                    read_in(connect_d, buf, sizeof(buf));
-                    if (strncasecmp("Oscar who?", buf, 10))
-                        say(connect_d, "You should say 'Oscar who?'!\r\n");
-                    else
-                        say(connect_d, "Oscar silly question, you get a silly answer\r\n");
+
+        if (fork() == 0) { // or if ( !fork())
+            // child process
+            close(listener_d);
+
+            if (say(connect_d, "Internet Knock-knock Protocol Server\r\nVersion I.O\r\nKnock!Knock!\r\n> ") != -1) {
+                read_in(connect_d, buf, sizeof(buf));
+                if( strncasecmp("Who's there?", buf, 12))
+                    say(connect_d, "You should say 'Who's there?'!");
+                else {
+                    if (say(connect_d, "Oscar\r\n> ") != -1) {
+                        read_in(connect_d, buf, sizeof(buf));
+                        if (strncasecmp("Oscar who?", buf, 10))
+                            say(connect_d, "You should say 'Oscar who?'!\r\n");
+                        else
+                            say(connect_d, "Oscar silly question, you get a silly answer\r\n");
+                    }
                 }
             }
+            close(connect_d);
+            // Once the child process has finished talking, it should exit.
+            // That will prevent it from falling into the main server loop.
+            exit(0);
         }
         close(connect_d);
+        
     }
     return 0;
 }
